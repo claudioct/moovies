bcd8fdc52c15f02438847a0b034f12b1879d48a5
Claudio Torres <claudioct@gmail.com>
2017-02-01 20:37:42 -0200

    Testes de unidade para recuperação de dados do csv

---
+++ Moovies.Tests/ImdbServiceTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moovies.Services;
+
+namespace Moovies.Tests
+{
+    [TestClass]
+    public class ImdbServiceTest
+    {
+        private string _csv;
+        private IImdbDataService _imdbService;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _csv = "\"position\",\"const\",\"created\",\"modified\",\"description\",\"Title\",\"Title type\",\"Directors\",\"You rated\",\"IMDb Rating\",\"Runtime (mins)\",\"Year\",\"Genres\",\"Num. Votes\",\"Release Date (month/day/year)\",\"URL\"\r\n\"1\",\"tt1255953\",\"Sat Jan 28 00:00:00 2017\",\"\",\"\",\"Incêndios\",\"Feature Film\",\"Denis Villeneuve\",\"10\",\"8.2\",\"131\",\"2010\",\"drama, mystery, war\",\"88199\",\"2010-09-03\",\"http://www.imdb.com/title/tt1255953/\"\r\n\"2\",\"tt4698684\",\"Sat Jan 21 00:00:00 2017\",\"\",\"\",\"A Incrível Aventura de Rick Baker\",\"Feature Film\",\"Taika Waititi\",\"9\",\"7.9\",\"101\",\"2016\",\"adventure, comedy, drama\",\"40156\",\"2016-01-22\",\"http://www.imdb.com/title/tt4698684/\"";
+
+            _imdbService = new ImdbDataService();
+        }
+
+        [TestMethod]
+        public void Read_Id_Ok()
+        {
+            var imdbMoviesCollection = _imdbService.GetRecords(_csv);
+            Assert.AreEqual("tt1255953", imdbMoviesCollection[0].Id);
+        }
+
+        [TestMethod]
+        public void Read_Title_Ok()
+        {
+            var imdbMoviesCollection = _imdbService.GetRecords(_csv);
+            Assert.AreEqual("Incêndios", imdbMoviesCollection[0].Title);
+        }
+
+        [TestMethod]
+        public void Read_TitleType_Ok()
+        {
+            var imdbMoviesCollection = _imdbService.GetRecords(_csv);
+            Assert.AreEqual("Feature Film", imdbMoviesCollection[0].TitleType);
+        }
+
+        [TestMethod]
+        public void Read_Directors_Ok()
+        {
+            var imdbMoviesCollection = _imdbService.GetRecords(_csv);
+            Assert.AreEqual("Denis Villeneuve", imdbMoviesCollection[0].Directors);
+        }
+
+        [TestMethod]
+        public void Read_UserRating_Ok()
+        {
+            var imdbMoviesCollection = _imdbService.GetRecords(_csv);
+            Assert.AreEqual(10, imdbMoviesCollection[0].UserRating);
+        }
+
+        [TestMethod]
+        public void Read_ImdbRating_Ok()
+        {
+            var imdbMoviesCollection = _imdbService.GetRecords(_csv);
+            Assert.AreEqual(8.2M, imdbMoviesCollection[0].ImdbRating);
+        }
+
+        [TestMethod]
+        public void Read_Runtime_Ok()
+        {
+            var imdbMoviesCollection = _imdbService.GetRecords(_csv);
+            Assert.AreEqual(131, imdbMoviesCollection[0].Runtime);
+        }
+
+        [TestMethod]
+        public void Read_Runtime_Year()
+        {
+            var imdbMoviesCollection = _imdbService.GetRecords(_csv);
+            Assert.AreEqual(2010, imdbMoviesCollection[0].Year);
+        }
+
+        [TestMethod]
+        public void Read_Runtime_Genres()
+        {
+            var imdbMoviesCollection = _imdbService.GetRecords(_csv);
+            Assert.AreEqual(new string[] { "drama", "mystery", "war" }[0], imdbMoviesCollection[0].Genres[0]);
+            Assert.AreEqual(new string[] { "drama", "mystery", "war" }[1], imdbMoviesCollection[0].Genres[1]);
+            Assert.AreEqual(new string[] { "drama", "mystery", "war" }[2], imdbMoviesCollection[0].Genres[2]);
+        }
+
+        [TestMethod]
+        public void Calculate_Total_Time()
+        {
+            var imdbData = _imdbService.Retrieve(_csv);
+            Assert.AreEqual(232, imdbData.TotalTime);
+        }
+    }
+}
